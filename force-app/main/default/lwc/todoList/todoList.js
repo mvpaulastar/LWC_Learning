5c56ee215ab73a1b2d8ca8832de92d27527bae44
mvpaulastar <paulaclark30@yahoo.com>
2023-03-15 16:32:41 -0500

    created todoList component

--- force-app/main/default/lwc/todoList/todoList.js
+++ force-app/main/default/lwc/todoList/todoList.js
@@ -1,5 +1,15 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 
 export default class TodoList extends LightningElement {
-  taskList = ["Dance", "Sleep", "Work", "Eat", "Go to sleep"];
+  @track taskList = ["Dance", "Sleep", "Work", "Eat", "Go to sleep"];
+  @track newTask;
+
+  handleAdd() {
+    const newTask = this.template.querySelector("lightning-input").value;
+    this.taskList.push(newTask);
+    console.log(this.taskList);
+  }
+  handleDelete(event) {
+    this.taskList.splice(event.detail.index, 1);
+  }
 }
