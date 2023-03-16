c921310869afd4eee604ec487fc16fa914ef30d1
mvpaulastar <paulaclark30@yahoo.com>
2023-03-16 12:31:23 -0500

    todoList changes.

--- force-app/main/default/lwc/todoList/todoList.js
+++ force-app/main/default/lwc/todoList/todoList.js
@@ -1,15 +1,31 @@
 import { LightningElement, track } from "lwc";
 
 export default class TodoList extends LightningElement {
-  @track taskList = ["Dance", "Sleep", "Work", "Eat", "Go to sleep"];
-  @track newTask;
+  @track taskList = [
+    {
+      id: 1,
+      task: "Dance"
+    },
+    {
+      id: 2,
+      task: "Eat"
+    },
+    {
+      id: 3,
+      task: "Sleep"
+    }
+  ];
+  newTask;
 
   handleAdd() {
-    const newTask = this.template.querySelector("lightning-input").value;
-    this.taskList.push(newTask);
-    console.log(this.taskList);
+    const newTask = {
+      id: this.taskList.length + 1,
+      task: this.template.querySelector("lightning-input").value
+    };
+    console.log(newTask);
+    this.taskList = [...this.taskList, newTask];
   }
   handleDelete(event) {
-    this.taskList.splice(event.detail.index, 1);
+    this.taskList.splice(event.target.value, 1);
   }
 }
